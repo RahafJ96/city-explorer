665fc2b63d429f0a51d26216f97ef1a7646bbe1e
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-08-04 19:34:33 +0300

    lab08 front-end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,109 @@
 import React from 'react';
 import Header from './components/Header';
-import Footer from './components/Footer.js';
-import Main from './components/Main.js';
+import Footer from './components/Footer';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Map from './components/Map';
+import Weather from './components/Weather';
+import Movies from './components/Movies';
 
 class App extends React.Component {
 
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      displayName: '',
+      lon: '',
+      lat: '',
+      errorMsg: 'Bad Response',
+      displayErr: false,
+      showMap: false,
+      showCard: false,
+      weather: [],
+      movies: []
+    }
+
+  }
+
+
+
+
+
+  getLocationData = async (event) => {
+    event.preventDefault();
+    const city = event.target.city.value;
+
+    const URL = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${city}&format=json`;
+    try {
+
+      let locationResult = await axios.get(URL)
+      this.setState({
+        displayName: locationResult.data[0].display_name,
+        lon: locationResult.data[0].lon,
+        lat: locationResult.data[0].lat,
+        showMap: true,
+        displayErr: false,
+      })
+
+      const urlServer = `${process.env.REACT_APP_SERVER}/getWeather?lat=${this.state.lat}&lon=${this.state.lon}&cityName=${city}`
+      let weatherResult = await axios.get(urlServer)
+      this.setState({
+        weather: weatherResult.data,
+        showCard: true
+      })
+      const urlMovies = `${process.env.REACT_APP_SERVER}/movies?city=${city}`
+      let moviesResult = await axios.get(urlMovies)
+      this.setState({
+        movies: moviesResult.data
+      })
+    }
+    catch {
+      this.setState({
+        showMap: false,
+        displayErr: true,
+        showCard: false
+      }
+      )
+    }
+
+    // let URL = `${process.env.REACT_APP_SERVER_URL}/weather?lat=${this.state.lat}&lon=${this.state.lon}&searchQuery=city`;
+    let weatherData = await axios.get(URL);
+    this.setState({
+      weatherInfoArr: weatherData.data
+    })
+    console.log(weatherData);
+  }
   render() {
-    return(
+    return (
       <>
-        <Header />
-        <Main />
-        <Footer />
+        <Header/>
+
+        <Form onSubmit={this.getLocationData} className="text-center formSet">
+          <Form.Label className='seachLabel'><h4>Search for a City here: </h4></Form.Label>
+          <input className="inputButton" type='text' placeholder='Enter City' name='city' />
+          <button className="buttonSearch btn-primary text-white btn-lg" type='submit'>Explore!</button>
+        </Form>
+
+        <Map
+          displayName={this.state.displayName}
+          lon={this.state.lon}
+          lat={this.state.lat}
+          showMap={this.state.showMap}
+          displayErr={this.state.displayErr}
+          errorMsg={this.state.errorMsg}
+          showCard={this.state.showCard}
+
+        />
+        <Weather showCard={this.state.showCard} weather={this.state.weather} ></Weather>
+        <Movies showCard={this.state.showCard} movies={this.state.movies} ></Movies>
+        <Footer/>
       </>
+
     )
+
   }
 }
 
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import {Col,Container,Row} from 'react-bootstrap';
+
+
 
 class Weather extends React.Component {
+
   render() {
-    return(
-      this.props.weather.map((item) => (
-        <div>
-          <p><h6>Date:</h6> {item.date}</p>
-          <p><h6>Description:</h6> {item.description}
-          <br/>____ ____ ____ </p>
-        </div>
-      ))
+    return (
+      <>
+
+        {
+          this.props.showCard && this.props.weather.map(element => {
+            return (
+              <Container fluid="md">
+                <Row >
+                  <Col>
+              <Card className="text-center" >
+                <Card.Body>
+                  <Card.Title>{element.date}</Card.Title>
+                  <Card.Text>{element.description}</Card.Text>
+                </Card.Body>
+              </Card>
+              </Col>
+                </Row>
+                </Container>
+
+            )
+          })
+        }
+
+
+      </>
     )
   }
+
+
 }
 
 export default Weather;
