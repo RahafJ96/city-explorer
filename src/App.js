24d2a765b620555ca28ca94c0e2ac2cfbb21376e
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-08-09 01:10:02 +0300

    city exploror

--- src/App.js
+++ src/App.js
@@ -48,13 +48,13 @@ class App extends React.Component {
         displayErr: false,
       })
 
-      const urlServer = `${process.env.REACT_APP_SERVER}/getWeather?lat=${this.state.lat}&lon=${this.state.lon}&cityName=${city}`
+      const urlServer = `http://localhost:3010/getWeather?lat=${this.state.lat}&lon=${this.state.lon}&cityName=${city}`
       let weatherResult = await axios.get(urlServer)
       this.setState({
         weather: weatherResult.data,
         showCard: true
       })
-      const urlMovies = `${process.env.REACT_APP_SERVER}/movies?city=${city}`
+      const urlMovies = `http://localhost:3010/movies?city=${city}`
       let moviesResult = await axios.get(urlMovies)
       this.setState({
         movies: moviesResult.data
